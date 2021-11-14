b685d68221724c5f18755cb6bb8b4e4efc10722c
Maxim DOVHOPOLYI <dovgopolyy0000@gmail.com>
2021-11-14 14:33:05 +0200

    fix: deploy

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 SERVICE_NAME=d2c-weave-cluster-manager
 SOURCE_DIR_NAME=/usr/lib/$SERVICE_NAME
 
+# clone/pull repo
 if [[ -d "$SOURCE_DIR_NAME" ]]
 then
   git -C $SOURCE_DIR_NAME pull
@@ -10,8 +11,17 @@ else
   git clone https://github.com/rollun-com/$SERVICE_NAME $SOURCE_DIR_NAME
 fi
 
+# setup env vars
+
+cat > "$SOURCE_DIR_NAME"/.env << EOL
+D2C_USER=$D2C_USER
+D2C_PASS=$D2C_D2C_PASS
+EOL
+
+# update libs
 npm i --prefix $SOURCE_DIR_NAME
 
+# update systemd service
 cp $SOURCE_DIR_NAME/systemd-service/$SERVICE_NAME.service /etc/systemd/system/$SERVICE_NAME.service
 
 systemctl daemon-reload
